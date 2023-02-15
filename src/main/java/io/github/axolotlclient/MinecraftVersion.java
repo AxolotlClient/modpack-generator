ab5df24d40486efbec297403b954981ce0a0ff6f
moehreag <moehreag@gmail.com>
2023-02-15 15:20:52 +0000

    remove jank

--- src/main/java/io/github/axolotlclient/MinecraftVersion.java
+++ src/main/java/io/github/axolotlclient/MinecraftVersion.java
@@ -9,24 +9,14 @@ public class MinecraftVersion {
     private final String[] mods;
     private final String loader;
 
-    private MinecraftVersion(String version, String... modSlugs) {
-        this.version = version;
-        this.mods = modSlugs;
-        this.loader = "quilt";
-    }
-
     private MinecraftVersion(String version, String loader, String... mods){
         this.version = version;
         this.loader = loader;
         this.mods = mods;
     }
 
-    public static MinecraftVersion of(String version, boolean thisIsUnUsed, String loader, String... mods){
+    public static MinecraftVersion of(String version, String loader, String... mods){
         return new MinecraftVersion(version, loader, mods);
-    }
-
-    public static MinecraftVersion of(String version, String... mods){
-        return new MinecraftVersion(version, mods);
     }
 
     public String getVersion(){
@@ -41,22 +31,22 @@ public class MinecraftVersion {
         return loader;
     }
 
-    public static MinecraftVersion MC1193 = MinecraftVersion.of("1.19.3",
+    public static MinecraftVersion MC1193 = MinecraftVersion.of("1.19.3", "quilt",
             "qsl", "sodium", "lithium", "starlight",
             "lambdabettergrass", "axolotlclient", "lambdynamiclights", "modmenu",
             "lazydfu", "iris", "ferrite-core", "indium", "polymer", "dynamic-fps", "sodium-extra", "reeses-sodium-options",
             "no-telemetry");
-    public static MinecraftVersion MC1192 = MinecraftVersion.of("1.19.2",
+    public static MinecraftVersion MC1192 = MinecraftVersion.of("1.19.2", "quilt",
             "qsl", "sodium", "lithium", "starlight",
             "lambdabettergrass", "lambdynamiclights", "axolotlclient", "modmenu",
             "lazydfu", "iris", "ferrite-core", "indium", "polymer", "dynamic-fps", "sodium-extra", "reeses-sodium-options",
             "no-telemetry");
-    public static MinecraftVersion MC1165 = MinecraftVersion.of("1.16.5", true, "fabric",
+    public static MinecraftVersion MC1165 = MinecraftVersion.of("1.16.5", "fabric",
             "fabric-api", "axolotlclient", "sodium", "lithium", "modmenu", "no-telemetry", "lambdynamiclights",
             "lambdabettergrass");
     /*public static MinecraftVersion MC116_combat8c = MinecraftVersion.of("1.16_combat-6", false, "fabric", // NOT AVAILABLE ON MODRINTH
             "axolotlclient", "fabric-api");*/
-    public static MinecraftVersion MC189 = MinecraftVersion.of("1.8.9", false, "fabric",
+    public static MinecraftVersion MC189 = MinecraftVersion.of("1.8.9", "fabric",
             "legacy-fabric-api", "axolotlclient");
 
     public static Set<MinecraftVersion> versions = Set.of(MC1193, MC1192, MC1165, MC189);
